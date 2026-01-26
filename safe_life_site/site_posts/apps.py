4a1635e52a3c370bdb9f617df866bcc1b52b0820
Cheef44 <cheef44.prog@gmail.com>
2026-01-26 14:29:55 +0300

    feature: базовые настройки django, базовые приложения

---
+++ safe_life_site/site_posts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SitePostsConfig(AppConfig):
+    name = 'site_posts'
---
+++ safe_life_site/site_users/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SiteUsersConfig(AppConfig):
+    name = 'site_users'
