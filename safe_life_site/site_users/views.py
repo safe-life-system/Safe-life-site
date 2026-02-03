4a07d43f915258c361cca088be8a981aeabda798
Cheef44 <cheef44.prog@gmail.com>
2026-02-03 23:43:23 +0300

    feature(views): реализация функции регистрации и функции входа

--- safe_life_site/site_users/views.py
+++ safe_life_site/site_users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import UserReg
 
 # Create your views here.
+
+def user_regiistration(request):
+    if request.method == 'POST':
+        form = UserReg(request.POST)
+        if form.is_valid():
+            form.save()
+            user = authenticate(request, username=request.POST.get("username"), password=request.POST.get("password1"))
+            if user is not None:
+                login(request=request, user=user)
+                return redirect('/')
+    form = UserReg()
+    return render(request, "user_registration/user_registration.html", {"user_form":form})
+
+def user_login(request):
+    if request.method == 'POST':
+        user_name = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=user_name, password=password)
+        if user is not None:
+            login(request=request, user=user)
+            return redirect('/')
+    return render(request, "user_registration/user_login.html")
