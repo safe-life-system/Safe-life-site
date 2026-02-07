3bc2ca1a68d584816cfde3213fd437ee54e2eb83
Cheef44 <cheef44.prog@gmail.com>
2026-02-08 01:26:56 +0300

    feature(views): реализация функции создания поста

--- safe_life_site/site_posts/views.py
+++ safe_life_site/site_posts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Branches, Posts, Images
+from .form import PostImageDawnlod, PostEdit
+from django.utils import timezone
 
 # Create your views here.
+
+#Функция добавения поста
+def add_post(request):
+    if request.method == "POST":
+        post_edit = PostEdit(request.POST, request.FILES)
+        post_image = PostImageDawnlod(request.POST, request.FILES)
+        if post_edit.is_valid() and post_image.is_valid():
+            form = post_edit.save(commit=False)
+            form.author = request.user
+            form.date = timezone.now()
+            form.save()
+            return redirect("/")
+    else:
+        form = PostEdit()
+    return render(request, 'post_create.html', {'form':form})
