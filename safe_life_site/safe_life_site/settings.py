8255e69d9bc166c5679e1d127ed19490cb03e11c
Cheef44 <cheef44.prog@gmail.com>
2026-02-09 18:34:40 +0300

    mend

--- safe_life_site/safe_life_site/settings.py
+++ safe_life_site/safe_life_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tdezmm5#f9di+*q)-_-jf%gwblu+qc@+j#x+zqv)7fmx+y^ii='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "safe-life-site.ru", "www.safe-life-site.ru"]
 
