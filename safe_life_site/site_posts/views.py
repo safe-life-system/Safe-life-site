b776262434c834d849d88024de45b38028059316
Cheef44 <cheef44.prog@gmail.com>
2026-05-05 20:22:44 +0300

    feature: замена поиска по id, на поиск по slug

--- safe_life_site/site_posts/views.py
+++ safe_life_site/site_posts/views.py
@@ -4,6 +4,7 @@ from .models import Branches, Posts, Images
 from .form import PostImageDawnlod, PostEdit
 from django.utils import timezone
 import markdown
+from django.utils.text import slugify
 
 # Create your views here.
 #Функция добавения поста
@@ -25,23 +26,24 @@ def add_post(request):
     return render(request, 'post_create.html', {'form':form, 'form_image':form_image})
 
 #Функция вывода постов
-def posts(request, id):
-    branche = get_object_or_404(Branches, pk = id)
+def posts(request, slug):
+    branche = get_object_or_404(Branches, slug = slug)
+    print(branche.name_branche)
     posts_data = Posts.objects.filter(branche__name_branche=branche).order_by('-date')
     for post in posts_data:
         post.main_text = markdown.markdown(post.main_text, extensions=['fenced_code', 'codehilite'])
-    return render(request, 'posts.html', {'posts': posts_data})
+    return render(request, 'posts.html', {'branche': branche,'posts': posts_data})
 
 #Функция вывода деталей поста
-def post_detail(request, pk):
-    post = get_object_or_404(Posts, pk=pk)
+def post_detail(request, slug):
+    post = get_object_or_404(Posts, slug=slug)
     post.main_text = markdown.markdown(post.main_text, extensions=['fenced_code', 'codehilite'])
     post.text = markdown.markdown(post.text, extensions=['fenced_code', 'codehilite'])
-    image = Images.objects.filter(posts_id=pk)
+    image = Images.objects.filter(posts=post)
     return render(request, "post_detail.html", {'post':post, 'image':image})
 
-def post_edit(request, id):
-    post = get_object_or_404(Posts, pk=id)
+def post_edit(request, slug):
+    post = get_object_or_404(Posts, slug=slug)
     if request.method == "POST":
         form_post = PostEdit(request.POST, request.FILES, instance=post)
         form_image = PostImageDawnlod(request.POST, request.FILES, instance=post)
@@ -52,7 +54,7 @@ def post_edit(request, id):
             form.save()
             for file in request.FILES.getlist('image'):
                 Images.objects.create(posts=form, image=file)
-            return redirect('/')
+            return redirect('post_detail', slug=post.slug)
     form = PostEdit(instance=post)
     form_image = PostImageDawnlod(instance=post)
     return render(request, 'post_create.html', {'form':form, 'form_image':form_image})
