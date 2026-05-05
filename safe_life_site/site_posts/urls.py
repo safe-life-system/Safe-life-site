0b458514bfaa19d2e8d229af5d7f0ace06016b9a
Cheef44 <cheef44.prog@gmail.com>
2026-05-05 20:23:26 +0300

    feature: замена id на slug в urls

--- safe_life_site/site_posts/urls.py
+++ safe_life_site/site_posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns_posts = [
     path('post_create/', views.add_post, name='post_create'), 
-    path('posts/<int:id>/', views.posts, name='posts_data'),
-    path('posts_detail/<int:pk>', views.post_detail, name='post_detail'),
-    path('post/editer/<int:id>', views.post_edit, name='post_edit')
+    path('posts/<slug:slug>/', views.posts, name='posts_data'),
+    path('posts_detail/<slug:slug>', views.post_detail, name='post_detail'),
+    path('post/editer/<slug:slug>', views.post_edit, name='post_edit')
 ]
