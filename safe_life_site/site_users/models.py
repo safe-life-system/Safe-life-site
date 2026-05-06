24229505ef96d752f644d530d88aceca9f63b69c
Cheef44 <cheef44.prog@gmail.com>
2026-05-06 23:31:47 +0300

    feature(models): реализация модели комментариев

--- safe_life_site/site_users/models.py
+++ safe_life_site/site_users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from site_posts.models import Posts
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    user_name = models.ForeignKey(User, models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.text
