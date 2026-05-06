550f470217882ff07a9d8f333643573324fe1c93
Cheef44 <cheef44.prog@gmail.com>
2026-05-06 18:21:34 +0300

    feature(urls): реализация редиректа старых url

--- safe_life_site/site_posts/urls.py
+++ safe_life_site/site_posts/urls.py
@@ -3,8 +3,11 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns_posts = [
+    path('posts_detail/<int:pk>/', views.old_post_detail_redirect, name='old_post_detail_redirect'),
+    path('posts/<int:pk>/', views.old_posts_redirect, name='old_posts_redirect'),
     path('post_create/', views.add_post, name='post_create'), 
     path('posts/<slug:slug>/', views.posts, name='posts_data'),
     path('posts_detail/<slug:slug>', views.post_detail, name='post_detail'),
-    path('post/editer/<slug:slug>', views.post_edit, name='post_edit')
+    path('post/editer/<slug:slug>', views.post_edit, name='post_edit'),
+    
 ]
