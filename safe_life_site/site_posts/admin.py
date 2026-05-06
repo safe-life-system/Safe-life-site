8d06912fa28b8dcea5acb2cdc6c03277057b77f6
Cheef44 <cheef44.prog@gmail.com>
2026-05-06 23:33:12 +0300

    feature(admin): добавление базы данных комментариев в админку

--- safe_life_site/site_posts/admin.py
+++ safe_life_site/site_posts/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib import admin
 from .models import Branches, Posts, Images
+from site_users.models import Comments
 
 # Register your models here.
 admin.site.register(Posts)
 admin.site.register(Branches)
 admin.site.register(Images)
+admin.site.register(Comments)
